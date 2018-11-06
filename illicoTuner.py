a99c28bbb1db20985c9354a3944f08ef7fb1ebcc
Jean-Manuel Gagnon <jean_manu@icloud.com>
2018-11-05 21:58:37 -0500

    Add remuxed fonction for Plex
    Add remuxed fonction for Plex and some clean-up.
    
    Now plex can connect to illicoTuner and ffmpeg will remux to ts

--- illicoTuner.py
+++ illicoTuner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from gevent import monkey; monkey.patch_all()
 import os
+import re
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -22,7 +23,10 @@ except ImportError:
     from cgi import parse_qs
 from urlparse import urlparse
 from pathlib import Path
-
+import subprocess
+import platform
+import socket
+import youtube_dl
 old_init = ssl.SSLSocket.__init__
 
 @functools.wraps(old_init)
@@ -32,18 +36,61 @@ def ubuntu_openssl_bug_965371(self, *args, **kwargs):
 
 ssl.SSLSocket.__init__ = ubuntu_openssl_bug_965371
 
+app = Flask(__name__)
+
+config = {
+    'tunerCount': 6,  # number of tuners
+    'chunkSize': 1024*1024
+}
+
+# LINUX/WINDOWS/OSX
+if platform.system() == 'Linux':
+	FFMPEGLOC = '/usr/bin/ffmpeg'
+
+elif platform.system() == 'Windows':
+	FFMPEGLOC = os.path.join('C:\FFMPEG', 'bin', 'ffmpeg.exe')
+
+elif platform.system() == 'Darwin':
+	FFMPEGLOC = '/usr/local/bin/ffmpeg'
+else:
+	log("Unknown OS detected... proxy may not function correctly")
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+        IP = 'http://' + IP
+    return IP
+    
+discoverData = {
+    'FriendlyName': 'illicoTuner',
+    'Manufacturer' : 'Videotron',
+    'ModelNumber': 'HDTC-2US',
+    'FirmwareName': 'hdhomeruntc_atsc',
+    'TunerCount': int(config['tunerCount']),
+    'FirmwareVersion': '20150826',
+    'DeviceID': '87654321',
+    'DeviceAuth': '44563690',
+    'BaseURL': '%s' % get_ip(),
+    'LineupURL': '%s/lineup.json' % get_ip()
+}
+
 COOKIE = os.path.join('', 'cookie')
 COOKIE_JAR = cookielib.LWPCookieJar(COOKIE)
 DEBUG = 'true'
 PROXYPORT = 5024
-URLHOST = 'http://127.0.0.1:' + str(PROXYPORT)
-
-app = Flask(__name__)
+URLHOST = get_ip() + ":" + str(PROXYPORT)
 
 def log(string):
-    if DEBUG == 'true':
-        print("[illicoTuner]: %s" %(string))
-        
+	if DEBUG == 'true':
+		print("[illicoTuner]: %s" %(string))
+
 def sessionCheck():
     log('SessionCheck: In progress...')
 
@@ -161,6 +208,26 @@ def getRequestedUrl(url, data=None, headers=None, params=None):
         code = response.getcode()
     return (data, code)
 
+def create_lineup():
+	lineupTuner = []
+	url = 'https://illicoweb.videotron.com/illicoservice/channels/user?localeLang=fr'
+	headers = {'User-agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0', 'Referer' : 'https://illicoweb.videotron.com/accueil'}
+	values = {}
+	jsonData, data = getRequest(url)
+	jsonData = (jsonData.encode('utf-8'))
+	channels = json.loads(jsonData)['body']['main']
+	for channel in channels:
+		title = channel['name']
+		pid = channel['defaultChannel']
+		live = str(channel['hasLinearFeed'])
+		if live == 'True':
+			log("Get channel: %s" %title)
+			urlPid = URLHOST + '/channel/remuxed?pid=' + str(pid)
+			lineupTuner.append({'GuideNumber': str(pid), 'GuideName': title, 'URL': urlPid })
+	with open("illicoTuner.json","w") as f:
+		json.dump(lineupTuner, f, ensure_ascii=False)
+	return lineupTuner
+
 @app.route('/credentials' , methods=['GET', 'POST'])
 def credentials():
 	credentials_Info = {}
@@ -172,8 +239,8 @@ def credentials():
 			USERNAME = credentials_Info['username']
 			PASSWORD = credentials_Info['password']
 		else:
-			USERNAME = request.form.get('username')
-			PASSWORD = request.form.get('password')
+			USERNAME = ''
+			PASSWORD = ''
 			credentials_Info = {
 								'username' : USERNAME,
 								'password' : PASSWORD
@@ -186,15 +253,85 @@ def credentials():
                   <input type="submit" value="Submit"><br>
               </form>'''
 
+@app.route('/discover.json')
+def discover():
+	return jsonify(discoverData)
+
+
+@app.route('/lineup_status.json')
+def status():
+    return jsonify({
+        'ScanInProgress': 0,
+        'ScanPossible': 1,
+        'Source': "cable",
+        'SourceList': ['Cable']
+    })
+
+
+@app.route('/lineup.json')
+def lineup():
+	json_File = Path("illicoTuner.json")
+	if not json_File.exists():
+		create_lineup()
+	with open('illicoTuner.json','r') as f:
+		json_Lineup = f.read()
+	return json_Lineup
+
+@app.route('/lineup.post', methods=['GET', 'POST'])
+def lineup_post():
+	create_lineup()
+	return redirect('/lineup.json')
 
 @app.route('/channel')
 def channel():
 	pid = request.args.get('pid', default = 1, type = str)
 	link_Pid = '/order/channel/' + str(pid)
-	log('link_Pid: %s' % link_Pid)
+	#log('link_Pid: %s' % link_Pid)
 	channel_Url = get_Live_Url(link_Pid)
 	log('channel_Url: %s' % channel_Url)
 	return redirect(channel_Url)
+
+@app.route('/channel/remuxed')
+def channel_Remuxed():
+	pid = request.args.get('pid', default = 1, type = str)
+	link_Pid = '/order/channel/' + str(pid)
+	channel_Url = get_Live_Url(link_Pid)
+	ydl_opts = {
+		'format': 'bestaudio/best',
+	}
+    
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		info_dict = ydl.extract_info(channel_Url, download=False)
+		video_url = info_dict.get("url", None)
+	def generate():
+		cmdline = list()
+		cmdline.append(FFMPEGLOC)
+		cmdline.append("-i")
+		cmdline.append(video_url)
+		cmdline.append("-vcodec")
+		cmdline.append("copy")
+		cmdline.append("-acodec")
+		cmdline.append("copy")
+		cmdline.append("-f")
+		cmdline.append("mpegts")
+		cmdline.append("pipe:1")
+		log(cmdline)
+		FNULL = open(os.devnull, 'w')
+		proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=FNULL)
+		try:
+				f = proc.stdout
+				byte = f.read(256)
+				while byte:
+					yield byte
+					byte = f.read(256)
+		finally:
+			proc.kill()
+
+
+	return Response(response=generate(), status=200, mimetype='video/mp2t',
+					headers={'Access-Control-Allow-Origin': '*', "Content-Type": "video/mp2t",
+							 "Content-Disposition": "inline", "Content-Transfer-Enconding": "binary"})
+
 
 @app.route('/M3u.get', methods=['GET', 'POST'])
 def create_M3u_List():
@@ -219,6 +356,11 @@ def create_M3u_List():
 	with open("illico.m3u","r") as f:
 		m3u_File = f.read()
 	return m3u_File
+
+@app.route('/')
+@app.route('/device.xml')
+def device():
+    return render_template('device.xml',data = discoverData),{'Content-Type': 'application/xml'}
 
 
 def get_Live_Url(pid):
